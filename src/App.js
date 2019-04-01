ddc50f5a87039514ce8c31d4abee4bb6ddfdaf11
Lange <lelange@deloitte.com>
2019-04-01 14:27:42 +0100

    added travis continuous integration

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Not at all !
+            Make this another new change
           </p>
           <a
             className="App-link"
