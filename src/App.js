9660c4e7c50e93b222064d3d16e861ad9c8e6e8f
Lange <lelange@deloitte.com>
2019-04-03 12:57:49 +0100

    ev

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit 01 feature! Extended..
+            Edit 01 feature! Extended version ...
           </p>
           <a
             className="App-link"
